b2b85a9ab3932b7500a62fd3780e39036490af22
Ramanujam Gond <ramanujamgond@gmail.com>
2023-08-23 20:27:19 +0530

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import UserResults from "../components/users/UserResults";
+
+const Home = () => {
+    return (
+        <>
+            {/* Search Components */}
+            <UserResults />
+        </>
+    )
+}
+
+export default Home
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
+  plugins: [require("daisyui")],
+}
+
